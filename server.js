548a29576a065d29c44e22770f9cf06f3cdfd033
uaelectronics-rikon <rikon@uaelectronicsindia.com>
2026-04-21 04:13:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PDFDocument = require("pdfkit");
 require('dotenv').config();
 
 const app = express();
-
+app.use(express.static('public'));
+ 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
